1f3681fc7e07c61d5fb17df8d590c7a2854357b0
=sofysha91 <sofy.hernandeza@gmail.com>
2022-06-29 22:14:13 -0500

    Class 01

---
+++ js/base.js
@@ -0,0 +1,21 @@
+console.log("Hello World!")
+
+//This is a comment
+/* This is a 
+multiline comment */
+
+//Datos en JS
+/*
+    1. Regla: No debe contener caracteres especiales
+    2. Regla: No inician con digitos
+    3. Regla: No acepta palabras reservadas
+    4. Buena practica: camelCase 99% - PascalCase -> clases - constante
+    5. Buena practica: naming descriptivo
+    6. Consideracion: Case sensitive
+    7. Recomendacion / Buena practica: Siempre usar let
+    8. Recomendacion / Buena practica: Siempre usar semicolon
+*/
+
+
+let my_variable = 10
+console.log(my_variable)
