8d7f8d2eafe2292f812c2a8da1c8d78e6c01e93b
=sofysha91 <sofy.hernandeza@gmail.com>
2022-06-30 22:14:36 -0500

    Adding parseInt

--- js/class2.js
+++ js/class2.js
@@ -1,6 +1,6 @@
 let celsius;
 let fahrenheit;
 
-celsius = prompt("Ingrese temperatura en °Celsius");
-fahrenheit = (celsius * (9/5)) + 32;
+celsius = prompt("Ingrese temperatura en °C");
+fahrenheit = (parseInt(celsius) * (9/5)) + 32;
 console.log("La temperatura en °F es:", fahrenheit);
