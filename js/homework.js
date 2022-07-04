52e08aa7ec8b6cf350ba07d7c46dd570120b7ea0
=sofysha91 <sofy.hernandeza@gmail.com>
2022-07-04 12:39:48 -0500

    Class 03 and homework

---
+++ js/homework.js
@@ -0,0 +1,13 @@
+//Ejercicio 1
+/*let num = parseInt(prompt("Ingrese numero"));
+
+let result = num % 2 == 0 ? `El numero ${num} es par` : `El numero ${num} es impar`; 
+console.log(result);*/
+
+
+//Ejercicio 2
+
+let base = parseInt(prompt("Ingrese base"));
+let altura = parseInt(prompt("Ingrese altura"));
+
+console.log(`El area del triangulo es: ${(base*altura) / 2}`);
