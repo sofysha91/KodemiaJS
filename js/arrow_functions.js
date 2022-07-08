94711dbc8fe0a0de99a2b29b860fb77f1deb128a
=sofysha91 <sofy.hernandeza@gmail.com>
2022-07-07 22:15:44 -0500

    Class 07

---
+++ js/arrow_functions.js
@@ -0,0 +1,9 @@
+//! Funciones flecha (Arrow functions)
+
+//? No necesitamos la palabra reservada function
+//? No en todos los casos es necesario la palabra return para regresar datos
+
+const arrowFunction = (param1, param2) => {
+    console.log(param1, param2);
+    return;
+}
